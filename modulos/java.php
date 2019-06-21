6cf33b233d4cc629bf0a08a078fe103fa3a5b4fb
YonathanR11 <yonathanr11.12@gmail.com>
2019-06-21 15:34:44 -0500

    modal add

--- modulos/java.php
+++ modulos/java.php
@@ -15,7 +15,8 @@ require_once "conexion.php";
           <span class="badge badge-dark">'.date_format($date, 'd/m/Y').'</span>
           <pre class="language-'.$row["lenguaje"].' line-numbers"><code>'.str_replace("<", "&lt;", $row["codigo"]).'</code></pre>';
         if (isset($_SESSION['usuario']) && $row['usuario'] == $_SESSION['usuario']) {
-            echo'<a href="#!" class="btn btn-warning">Editar</a>';
+          // echo '<button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#ModalEditar">NUEVA NOTA </button>';
+          echo '<button type="button" class="btn btn-warning" data-toggle="modal" data-target="#ModalEditar">Editar</button>';
             echo'<a href="#!" class="btn btn-danger ml-1">Borrar</a>';
         }
         echo '</div>
